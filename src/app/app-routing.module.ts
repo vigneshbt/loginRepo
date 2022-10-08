cb7c8d9e57f34d08ffa2252eaeee3c2ac92bd493
Sivabalan Rajagopal <sivabalan.rajagopal@xotiv.com>
2022-10-08 15:40:00 +0530

    Added first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+const routes: Routes = [
+  {path:'signin',component:SigninComponent},
+  {path:'signup',component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents =[SigninComponent,SignupComponent]
